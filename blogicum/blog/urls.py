c447b943569cce697825a4e3a4dff96590bbadc5
ArgentumX <email>
2025-05-19 21:35:48 +0300

    feat: init commit

---
+++ blogicum/blog/urls.py
@@ -0,0 +1,56 @@
+from . import views
+from django.urls import path
+
+app_name = "blog"
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='index'),
+    path(
+        'posts/create/',
+        views.CreatePostView.as_view(),
+        name='create_post'
+    ),
+    path(
+        'posts/<int:post_id>/',
+        views.PostDetailView.as_view(),
+        name='post_detail'
+    ),
+    path(
+        'posts/<int:post_id>/edit/',
+        views.PostEditView.as_view(),
+        name='edit_post'
+    ),
+    path(
+        'posts/<int:post_id>/delete/',
+        views.PostDeleteView.as_view(),
+        name='delete_post'
+    ),
+    path('category/<slug:category_slug>/',
+         views.CategoryPostView.as_view(), name='category_posts'),
+    path(
+        'profile/edit/',
+        views.ProfileEditView.as_view(),
+        name='edit_profile'
+    ),
+    path(
+        'posts/<int:post_id>/comment/',
+        views.CreateCommentView.as_view(),
+        name='add_comment'
+    ),
+    path(
+        'posts/<int:post_id>/edit_comment/<int:comment_id>/',
+        views.EditCommentView.as_view(),
+        name='edit_comment'
+    ),
+    path(
+        'posts/<int:post_id>/delete_comment/<int:comment_id>/',
+        views.DeleteCommentView.as_view(),
+        name='delete_comment'
+    ),
+    path(
+        'profile/<slug:username>/',
+        views.ProfileDetailView.as_view(),
+        name='profile'
+    ),
+
+]
