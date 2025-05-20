e2b026479d4f4d77336e4a11f48010c700411e81
ArgentumX <email>
2025-05-20 10:09:42 +0300

    fix: pep8 error

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,68 +1,71 @@
 from django.urls import path
-from . import views
+from .views import post_views
+from .views import comment_views
+from .views import profile_views
+
 app_name = "blog"
 
 urlpatterns_posts = [
     path(
         '',
-        views.PostListView.as_view(),
-        name='index'
+        post_views.PostListView.as_view(),
+        name='index',
     ),
     path(
         'posts/create/',
-        views.CreatePostView.as_view(),
-        name='create_post'
+        post_views.CreatePostView.as_view(),
+        name='create_post',
     ),
     path(
         'posts/<int:post_id>/',
-        views.PostDetailView.as_view(),
-        name='post_detail'
+        post_views.PostDetailView.as_view(),
+        name='post_detail',
     ),
     path(
         'posts/<int:post_id>/edit/',
-        views.EditPostView.as_view(),
-        name='edit_post'
+        post_views.EditPostView.as_view(),
+        name='edit_post',
     ),
     path(
         'posts/<int:post_id>/delete/',
-        views.DeletePostView.as_view(),
-        name='delete_post'
+        post_views.DeletePostView.as_view(),
+        name='delete_post',
     ),
     path(
         'category/<slug:category_slug>/',
-        views.CategoryPostView.as_view(),
-        name='category_posts'
+        post_views.CategoryPostView.as_view(),
+        name='category_posts',
     ),
 ]
 
 urlpatterns_comments = [
     path(
         'posts/<int:post_id>/comment/',
-        views.CreateCommentView.as_view(),
-        name='add_comment'
+        comment_views.CreateCommentView.as_view(),
+        name='add_comment',
     ),
     path(
         'posts/<int:post_id>/edit_comment/<int:comment_id>/',
-        views.EditCommentView.as_view(),
-        name='edit_comment'
+        comment_views.EditCommentView.as_view(),
+        name='edit_comment',
     ),
     path(
         'posts/<int:post_id>/delete_comment/<int:comment_id>/',
-        views.DeleteCommentView.as_view(),
-        name='delete_comment'
+        comment_views.DeleteCommentView.as_view(),
+        name='delete_comment',
     ),
 ]
 
 urlpatterns_profile = [
     path(
         'profile/edit/',
-        views.EditProfileView.as_view(),
-        name='edit_profile'
+        profile_views.EditProfileView.as_view(),
+        name='edit_profile',
     ),
     path(
         'profile/<slug:username>/',
-        views.ProfileDetailView.as_view(),
-        name='profile'
+        profile_views.ProfileDetailView.as_view(),
+        name='profile',
     ),
 ]
 
