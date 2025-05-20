94663d66c47d3847da65cb0f71ffcc310a509bd7
ArgentumX <email>
2025-05-20 10:05:05 +0300

    fix: pep8 error

--- blogicum/blog/views/__init__.py
+++ blogicum/blog/views/__init__.py
@@ -1,3 +1,36 @@
-from .post_views import *
-from .comment_views import *
-from .profile_views import *
+from .post_views import (
+    PostListView,
+    CreatePostView,
+    PostDetailView,
+    EditPostView,
+    DeletePostView,
+    CategoryPostView,
+)
+
+from .comment_views import (
+    CreateCommentView,
+    EditCommentView,
+    DeleteCommentView,
+)
+
+from .profile_views import (
+    EditProfileView,
+    ProfileDetailView,
+)
+
+__all__ = [
+    # post_views
+    'PostListView',
+    'CreatePostView',
+    'PostDetailView',
+    'EditPostView',
+    'DeletePostView',
+    'CategoryPostView',
+    # comment_views
+    'CreateCommentView',
+    'EditCommentView',
+    'DeleteCommentView',
+    # profile_views
+    'EditProfileView',
+    'ProfileDetailView',
+]
